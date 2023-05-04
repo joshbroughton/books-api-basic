8235d5a31fcdc95d2ddd17fd528899a1421cb2fd
= <=>
2023-05-04 09:19:16 -0600

    switched database to mongoDB atlas

--- src/config/db-setup.js
+++ src/config/db-setup.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 // connect to mongo db
 const mongoUri = process.env.MONGODB_URI
-mongoose.connect(mongoUri)
-
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`)
-})
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  })
+  .then(() =>  console.log('Connected to MongoDB'))
+  .catch(error  =>  console.error('Error connecting to MongoDB: ', error));
 
 module.exports = mongoose.connection
